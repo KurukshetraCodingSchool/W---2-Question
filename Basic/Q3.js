71b420d861a3e948e9547809034c7e00b422a5bf
kurukshetra <kurukshetracodingschool.com>
2025-07-30 20:38:19 +0530

    Q3 and Q4

---
+++ Basic/Q3.js
@@ -0,0 +1,14 @@
+// let c = Number(prompt());
+// let d =  Number(prompt());
+
+// let sum2 =  c+d;
+
+// console.log(sum2);
+// console.log(typeof(c));
+
+// Q4
+
+let name = prompt("Enter Your Name");
+let age =  Number(prompt("Enter Your Age"));
+
+console.log(`Hello,${name},You are ${age} year old`);
