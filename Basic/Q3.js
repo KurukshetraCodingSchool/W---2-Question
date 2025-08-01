d861b497244345864066526801d28ccba9dd6316
kurukshetra <kurukshetracodingschool.com>
2025-08-01 20:38:35 +0530

    Q6

--- Basic/Q3.js
+++ Basic/Q3.js
@@ -8,7 +8,19 @@
 
 // Q4
 
-let name = prompt("Enter Your Name");
-let age =  Number(prompt("Enter Your Age"));
+// let name = prompt("Enter Your Name");
+// let age =  Number(prompt("Enter Your Age"));
 
-console.log(`Hello,${name},You are ${age} year old`);
+// console.log(`Hello,${name},You are ${age} year old`);
+
+
+let var1 = 5;
+let var2 = 10;
+
+let temp ;
+
+temp =  var1;
+var1 = var2;
+var2 = temp;
+
+console.log(var1, var2);
